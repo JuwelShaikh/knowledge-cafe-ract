ace9b9cfda27759f06109591d7a422a81eb39d35
JuwelShaikh <juwelshaikh04@gmail.com>
2024-07-04 15:57:05 +0600

    Blog's json file added

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,7 @@ function App() {
 
   return (
     <>
-      
-        
-        
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hello beb</h1>
     </>
   )
 }
