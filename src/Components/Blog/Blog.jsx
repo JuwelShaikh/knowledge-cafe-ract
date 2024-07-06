0214cab43bd42be82aee66ac65af666552b6dccb
JuwelShaikh <juwelshaikh04@gmail.com>
2024-07-06 18:36:42 +0600

    Blogs side adjusted.

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
+import { CiBookmarkPlus } from "react-icons/ci";
+
 
 
 const Blog = ({ blog }) => {
-    const { title, cover, author_img, author, posted_date, reading_time } = blog;
+    const { title, cover, author_img, author, posted_date, reading_time, hashtags } = blog;
     return (
         <>
             <div className='my-4 border-b-2'>
-                <img src={cover} alt={`This is the picture of the ${title}`} />
+                <img className='w-full' src={cover} alt={`This is the picture of the ${title}`} />
                 <div className='flex justify-between my-2'>
                     <div className='flex'>
                         <img className='w-12' src={author_img} alt="Author img" />
@@ -17,10 +19,18 @@ const Blog = ({ blog }) => {
                     </div>
                     <div className='flex'>
                         <p>{reading_time} read time</p>
-                        {/* <button>btn</button> */}
+                        <button className='ml-2'><CiBookmarkPlus/></button>
                     </div>
                 </div>
-            <h1 className="text-xl ">{title}</h1>
+                <div className='mb-4'>
+                    <h1 className="text-xl ">{title}</h1>
+                    <p className='text-blue-700	'>
+                        {
+                            hashtags.map(hash => <span key={blog.id}><a href=''>#{hash} </a></span>)
+                        }
+                    </p>
+                    <a className='text-blue-700	 mb-5' href=""><u>Mark as read</u></a>
+                </div>
             </div>
         </>
     );
