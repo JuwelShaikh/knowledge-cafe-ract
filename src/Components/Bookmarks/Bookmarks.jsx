0c0344f0659c7b4e6843079c56347b3af39e9704
JuwelShaikh <juwelshaikh04@gmail.com>
2024-07-08 15:20:41 +0600

    Complete project

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -7,12 +7,12 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
         <>
             <div className="md:w-1/3">
                 <h4 className="bg-blue-50 text-center text-lg font-semibold text-blue-500 p-4 ml-2 mb-2 border border-indigo-600 rounded-md">Reading time spent: {readingTime}</h4>
-               <div className=" bg-gray-200 p-5 rounded-lg ml-2">
-               <h1 className="text-2xl text-center">Bookmarked blogs: {bookmarks.length}</h1>
-                {
-                    bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
-                }
-               </div>
+                <div className=" bg-gray-200 p-5 rounded-lg ml-2">
+                    <h1 className="text-2xl text-center">Bookmarked blogs: {bookmarks.length}</h1>
+                    {
+                        bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+                    }
+                </div>
             </div>
         </>
 
