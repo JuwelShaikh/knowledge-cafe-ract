a6382b716014b5258e0f8fcbebe9fdd2a0fb6f31
JuwelShaikh <juwelshaikh04@gmail.com>
2024-07-08 18:53:57 +0600

    Just a button added
    Dissapointed i

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -1,10 +1,13 @@
 
 
+
 const Bookmark = ({bookmark}) => {
     const {title} = bookmark
+
     return (
         <div>
             <h3 className="text-lg bg-white rounded-lg p-4 mx-1 my-4">{title}</h3>
+            <button className="btn-primary">Clear</button>
         </div>
     );
 };
